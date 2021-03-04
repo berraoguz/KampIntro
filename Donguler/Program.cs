04edbd2560acf8789a8e274d2d81a4bfd5a71ea3
Berra Oguz <berraoguz@outlook.com>
2021-03-04 23:01:47 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+
+            //array-dizi
+
+            string[] kurslar = new string[] {"Yazılım Geliştirici Yetiştirme Kampı", 
+                "Programlamaya Başlangıç İçin Temel Kurs", 
+                "Java", 
+                "Python", 
+                "C#" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("sayfa sonu - footer ");
+        }
+    }
+}
